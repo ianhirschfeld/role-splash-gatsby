59909223b6ed4e52e8e00eab7fc67a792be64f5d
Ian Hirschfeld <ianh7976@gmail.com>
2020-02-05 22:38:29 -0800

    Styles 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,20 @@
-import React from "react"
+import React from 'react'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+import styles from './404.module.css'
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title='404: Not found' />
+    <article className={styles.content}>
+      <h1 className={styles.logo}>Role</h1>
+      <h2 className='heading1'>Whoops!</h2>
+      <h3 className='heading2'>Looks like you're trying to travel off the table</h3>
+      <p>This is a non-existent page. In fact, there is only one page to this site. And it is <Link to='/'>right here</Link>.</p>
+    </article>
   </Layout>
 )
 
