5a8b18dc0684e0f5e7512733ac87ed322ae51cf8
Ian Hirschfeld <ianh7976@gmail.com>
2020-02-29 10:55:48 -0800

    Adds Google Analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: 'src/images/RoleFavicon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-159341071-1',
+        head: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
