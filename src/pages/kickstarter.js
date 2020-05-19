2bf1bcf83f071f9e30832a28f4dc6e88db31003c
Ian Hirschfeld <ianh7976@gmail.com>
2020-05-19 11:19:20 -0700

    Adds some referral pages.

---
+++ src/pages/kickstarter.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import Layout from '../components/layout'
+
+import styles from './404.module.css'
+
+export default ({ data }) => {
+  return (
+    <>
+      <Helmet>
+        <meta
+          http-equiv="refresh"
+          content="0;url=https://www.kickstarter.com/projects/role/role-a-new-kind-of-platform-for-the-rpg-community"
+        />
+      </Helmet>
+      <Layout articleClassNames={[styles.content]} isPlainFooter>
+        <h1 className={styles.logo}>Role</h1>
+        <h2 className="heading1">Redirecting you to our Kickstarter page...</h2>
+        <p>
+          If you aren't automatically redirected in 1 second,{' '}
+          <a href="https://www.kickstarter.com/projects/role/role-a-new-kind-of-platform-for-the-rpg-community">
+            click here
+          </a>
+          .
+        </p>
+      </Layout>
+    </>
+  )
+}
---
+++ src/pages/zyxx.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import Layout from '../components/layout'
+
+import styles from './404.module.css'
+
+export default ({ data }) => {
+  return (
+    <>
+      <Helmet>
+        <meta
+          http-equiv="refresh"
+          content="0;url=https://www.playrole.com/kickstarter?utm_source=maximumfun.org&utm_medium=podcast&utm_campaign=zyxx"
+        />
+      </Helmet>
+      <Layout articleClassNames={[styles.content]} isPlainFooter>
+        <h1 className={styles.logo}>Role</h1>
+        <h2 className="heading1">Thank you for your support!</h2>
+        <p>
+          You should be redirected to our Kickstarter in 1 second. If you aren't,{' '}
+          <a href="https://www.playrole.com/kickstarter?utm_source=maximumfun.org&utm_medium=podcast&utm_campaign=zyxx">
+            click here
+          </a>
+          .
+        </p>
+      </Layout>
+    </>
+  )
+}
