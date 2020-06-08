7628491edb2638b921d8445221f57a45a8552bc7
Ian Hirschfeld <ianh7976@gmail.com>
2020-06-08 09:59:55 -0700

    Adds longer refresh time for podcast referrals.

--- src/pages/kickstarter_storybreak.js
+++ src/pages/kickstarter_storybreak.js
@@ -10,14 +10,14 @@ export default ({ data }) => {
       <Helmet>
         <meta
           http-equiv="refresh"
-          content="0;url=https://www.kickstarter.com/projects/role/role-a-new-kind-of-platform-for-the-rpg-community?ref=d5v5fs"
+          content="3;url=https://www.kickstarter.com/projects/role/role-a-new-kind-of-platform-for-the-rpg-community?ref=d5v5fs"
         />
       </Helmet>
       <Layout articleClassNames={[styles.content]} isPlainFooter>
         <h1 className={styles.logo}>Role</h1>
         <h2 className="heading1">Thank you for your support!</h2>
         <p>
-          You should be redirected to our Kickstarter in 1 second. If you aren't,{' '}
+          You should be redirected to our Kickstarter in 3 seconds. If you aren't,{' '}
           <a href="https://www.kickstarter.com/projects/role/role-a-new-kind-of-platform-for-the-rpg-community?ref=d5v5fs">
             click here
           </a>
